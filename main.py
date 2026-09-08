7a4e4502018549dd4f4cf0f423109965787bf554
robotmannen <t.korsnes@hotmail.com>
2026-09-08 12:02:37 +0200

    Added ship movement

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
+from player import Player
 from pygame import display
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -10,6 +12,12 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    my_group = pygame.sprite.Group()
 
     while True:
         log_state()
@@ -17,7 +25,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+
+        player.update(dt)
+        player.draw(screen)
+
         display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
